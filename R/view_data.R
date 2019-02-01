2f9223044af8dd98003bf4a5a77e2e50a25d1dbf
Violeta Calian <VioletaC@hagstofa.local>
2019-02-01 12:06:34 +0000

    fix documentation flow, corrections to view_univar

--- R/view_data.R
+++ R/view_data.R
@@ -3,42 +3,45 @@
 #' short description of data: summaries and plots
 #' @param df input data
 #' ....other important comments
-#'  
+#'
 #' @import tidyverse
 #' @import magrittr
 #' @import DataExplorer
 #' @import funModeling
 #' @import ggplot2
-#' @import  rmarkdown 
+#' @import  rmarkdown
 #' @import  tabplot
-#' @examples overview_data(df = ggplot2::diamonds)
-#' 
+#' @import skimr
+#' @import ff
+#' @export
+#' @examples view_data(df = ggplot2::diamonds)
+#'
 #'
 
 view_data <- function(df, ...) {
-  
-  # make sure characters as factors 
-  df %<>% mutate_if(is.character, as.factor)   
-  
+
+  # make sure characters as factors
+   df %<>% dplyr::mutate_if(is.character, as.factor)
+
   #we may have other factors, too, represented by discrete values
-  
+
   #shortest overview
-  funModeling::df_status(df)  
+  funModeling::df_status(df)
   #few more details
-  skim(df) %>% skimr::kable()   
-  
+  skim(df) %>% skimr::kable()
+
   # continuous variables
   # using funModeling
-  funModeling::profiling_num(df)  
-  
-  # discrete 
-  # using dataExplorer 
-  DataExplorer::plot_bar(df)  
-  
+  funModeling::profiling_num(df)
+
+  # discrete
+  # using dataExplorer
+  DataExplorer::plot_bar(df)
+
   # from dataExplorer; useful names of variables: discrete and continuous
-  dnames <-names(split_columns(df)$discrete)      
-  cnames <- names(split_columns(df)$continuous)  
-  
+  dnames <-names(split_columns(df)$discrete)
+  cnames <- names(split_columns(df)$continuous)
+
   ###-------- to do --------------------------------------------------------------------------------------
   ### remove some features since too much missing data ***, ex
   # dat[, -which(colMeans(is.na(dat)) > 0.5)]
@@ -47,20 +50,25 @@ view_data <- function(df, ...) {
   #***********************
   ### reorder categories by some continuous variable when interesting ...
   #-----------------------------------------------------------------------
-  
-  DataExplorer::plot_intro(df)  
-  
-  DataExplorer::plot_missing(df)  
-  
+
+  DataExplorer::plot_intro(df)
+
+  DataExplorer::plot_missing(df)
+
   # table plots from tabplot package
-  plot_list <- lapply(cnames, FUN=function(x) {
-    tableplot(dat=df, sortCol=x)
+    # make ff package work correctly:
+    # with: set options(fftempdir = "path/to/your/folder") to a folder where you have access to
+  #getOption("fftempdir")
+    ##ff:setOptions("fftempdir" = getwd())
+  plot_list <-
+  lapply(cnames, FUN=function(x) {
+    tabplot::tableplot(dat=df, sortCol=x)
   }
   )
-  
-  
+
+
   ### to do:  save all these plots into a unique report file
-  
+
   #------------------------------------------------------------------------
-  
+
 }
--- R/view_univar.R
+++ R/view_univar.R
@@ -1,32 +1,33 @@
 
 #' view_univar function
-#' univariate analysis: distribution functions and outliers 
+#' univariate analysis: distribution functions and outliers
 #' @param df input data
 #' ....other important comments
-#'  
+#'
 #' @import tidyverse
 #' @import magrittr
 #' @import DataExplorer
 #' @import funModeling
 #' @import ggplot2
-#' @import  rmarkdown 
+#' @import  rmarkdown
+#' @export
 #' @examples overview_univar(df = ggplot2::diamonds)
-#' 
+#'
 #'
 #'
 view_univar <- function(df, ...) {
-  
-  
+
+
   # names of variables which are discrete and continuous, using funModeling
-  dnames <-names(split_columns(df)$discrete)     
-  cnames <- names(split_columns(df)$continuous)  
-  
+  dnames <-names(split_columns(df)$discrete)
+  cnames <- names(split_columns(df)$continuous)
+
   # univariate distributions, densities and cumulative, for discrete and continuous variables-------------------
-  # pdf's of numerical varaibles 
-  funModeling::plot_num(df, bins=10)  
+  # pdf's of numerical varaibles
+  funModeling::plot_num(df, bins=10)
   #or
-  plot_histogram(df)    
-  
+  plot_histogram(df)
+
   # Cumulative_df's of numerical variables
   # using points
   lapply(cnames, FUN=function(x) {
@@ -35,15 +36,15 @@ view_univar <- function(df, ...) {
   }
   )
   #or with lines
-  #ggplot(df, aes(depth)) + stat_ecdf(geom = "step")   
-  
+  #ggplot(df, aes(depth)) + stat_ecdf(geom = "step")
+
   # pdf's of categorical variables
   # plot_bar(df, maxcat=400L)
   DataExplorer::plot_bar(df)
-  
-  
-  
-  
+
+
+
+
   ##lapply(dnames, FUN = function(x) {
   ##   x <- df$cut
   #    df %>%
@@ -52,44 +53,44 @@ view_univar <- function(df, ...) {
   #             geom_bar()
   ##  }
   ##  )
-  
+
   # categorical ecdf not interesting
-  
-  
+
+
   # outliers, univariate -----------------------------------------------
-  # using Tukey 
+  # using Tukey
   lapply(cnames, FUN=function(x) {
     c(
       x,
-      funModeling::tukey_outlier(df$x)  
+      funModeling::tukey_outlier(df$x)
     )
-  } 
-  ) 
-  
+  }
+  )
+
   # using Hampel
   lapply(cnames, FUN=function(x) {
     c(
       x,
-      funModeling::hampel_outlier(df$x)  
+      funModeling::hampel_outlier(df$x)
     )
-  } 
-  ) 
-  
+  }
+  )
+
   ### ------ to do -----------------
   ##  add more tests for outliers ***
   ### ---------------------------------
-  
+
   # qqplots ----------------------------------------------------------------
   DataExplorer::plot_qq(df)
-  
+
   # boxplots ----------------------------------------------------------------
   #
   lapply(dnames, FUN=function(x) {
     DataExplorer::plot_boxplot(df, x )
   }
   )
-  
-  
-  
+
+
+
 }
 
