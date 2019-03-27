2984f6f60a49d5bef98f09257d32c25a1f6b8ca5
Violeta Calian <45428243+violetacln@users.noreply.github.com>
2019-03-27 15:43:56 +0000

    updat check_assumptions

--- R/check_assumptions.R
+++ R/check_assumptions.R
@@ -1,2 +1,15 @@
-# category: already included in modeling packages
-# identify needed "tests of tests"
+#' check_assumptions function
+#' checking main assumptions of statistical tests or reporting the results of the implicit checks of R functions
+#' @param df input data
+#' @param assumption the name of the assumption of the test
+#' param test_name the name of the statistical test
+#' @import tidyverse
+#' @import magrittr
+#' @export
+#' @examples check_assumptions(df = ggplot2::diamonds, assumption = ..., test_name= ...)
+#'
+#'
+
+check_assumptions <- function(df, assumption, test_name) {
+  #--------------
+  }
