9cc958e8a81baabd6e37a29ae46a32939dd0faa5
Violeta Calian <45428243+violetacln@users.noreply.github.com>
2019-03-27 15:33:09 +0000

    created first version of basic_variability

---
+++ R/basic_variability.R
@@ -0,0 +1,38 @@
+#' basic_variability function for general data sets
+@param df input data
+#' ...other important comments
+@import boot
+@import DataExplorer
+@export 
+@examples basic_variability(df=ggplot::diamonds)
+#' the main resource for this function is the package boot
+citation("boot")
+#' the basic methods of resampling with replacement, paramteric and nonparametric, are general enough to cover
+#' main statistics for general data sets and basic modeling formulae
+#' with confidence intervals and corresponding plots 
+#' for details one is referred to the boot package manual
+
+basic_variability <- function(df, ...) {
+
+
+#---------------------- continuous variables
+dnames <- names(DataExplorer::split_columns(df)$discrete)
+cnames <- names(DataExplorer::split_columns(df)$continuous)
+
+set.seed(111)
+lapply(cnames, FUN=function(var) {
+        stats <- c(mean(var),median(var), sd(var), skewness(var))
+                        # to introduce naming of these dimensions for reporting
+        res <- boot::boot(data=df,statistics=stats, R=1000)
+        lapply(c(1:length(stats)), FUN=function(i) {
+                    plot(res, index=i)
+                    boot.ci(res, type="bca", index=i)
+                    }
+  }        
+
+
+#---------------------- categorical variables
+# use Shannon's entropy for example as a measure
+
+
+}
