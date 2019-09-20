6cba40ad67b8b536d57817a9d64169166a2f5f6f
Violeta Calian <VioletaC@hagstofa.local>
2019-09-20 13:32:16 +0000

    small corrections

--- tests/testthat/test_ex.R
+++ tests/testthat/test_ex.R
@@ -4,30 +4,31 @@ library(testthat)
 
 # as seen on:
 # https://github.com/yihui/testit/blob/master/man/assert.Rd
-# we can try examples like
 
-has_warning(2-3)
+# we can try examples like and build our own
 
-has_error(1+"a")
-has_error(stop("err"), silent=TRUE)
+#has_warning(2-3)
 
-assert('A non-exported function works', {
-  res = utility_foo(x = 'abcd', y = 1:100)
-  (is.character(res))
-})
+#has_error(1+"a")
+#has_error(stop("err"), silent=TRUE)
+
+#assert('A non-exported function works', {
+#  res = utility_foo(x = 'abcd', y = 1:100)
+#  (is.character(res))
+#})
 
 
-try(assert("logical(0) cannot pass", 1 == integer(0)))
-stopifnot(1 == integer(0)) # it's OK!
+#try(assert("logical(0) cannot pass", 1 == integer(0)))
+#stopifnot(1 == integer(0)) # it's OK!
 
 # a compound expression
-try(assert("this if statement returns TRUE", if (TRUE) {
-  x = 1
-  x == 2
-}))
+#try(assert("this if statement returns TRUE", if (TRUE) {
+#  x = 1
+#  x == 2
+#}))
 
 # no message
-assert(!FALSE, TRUE, is.na(NA))
+#assert(!FALSE, TRUE, is.na(NA))
 
 #test_pkg()
 
